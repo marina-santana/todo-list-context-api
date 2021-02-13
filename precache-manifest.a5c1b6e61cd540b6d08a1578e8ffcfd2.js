0d743a1a2b1bf8143bcc0a7c1b4e702e9ceed5dc
marina-santana <47863089+marina-santana@users.noreply.github.com>
2021-02-13 00:15:14 +0000

    Deploying to gh-pages from @ marina-santana/todo-list-context-api@3461343150cb0d734190e76515c6a79d1c5c6f26 🚀

---
+++ precache-manifest.a5c1b6e61cd540b6d08a1578e8ffcfd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e641c0288d802ddcb6c529f5b6668de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c018b2187a569187161",
+    "url": "/static/css/main.7eceb108.chunk.css"
+  },
+  {
+    "revision": "bc003b607ee57a60e697",
+    "url": "/static/js/2.86d94815.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.86d94815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c018b2187a569187161",
+    "url": "/static/js/main.72571388.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.878930be840d999492d946e83e197f88.js"
+  "/precache-manifest.a5c1b6e61cd540b6d08a1578e8ffcfd2.js"
 );
 
 self.addEventListener('message', (event) => {
